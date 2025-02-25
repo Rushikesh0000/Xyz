c67b1c43497bff7d0a4be79bd6e1f7a95f8587fa
Rushikesh0000 <129922707+Rushikesh0000@users.noreply.github.com>
2025-02-25 16:07:02 +0530

    Save

---
+++ Xyz/src/abc/Abc.java
@@ -0,0 +1,11 @@
+package abc;
+
+public class Abc {
+	public static void main(String[]x)
+	{
+		String s="ABC";
+		
+		System.out.println(s);
+	}
+
+}
---
+++ Xyz/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author prush
+ *
+ */
+module ABC {
+}
